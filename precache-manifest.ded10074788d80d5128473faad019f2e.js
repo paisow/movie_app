f4f6634506f5a81511b4f2c6a948aa21feda1afb
paisow <paisow@gmail.com>
2020-01-26 22:45:26 +0900

    Updates

---
+++ precache-manifest.ded10074788d80d5128473faad019f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aaf4cc81922b3c19659cb310763ee49",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5b296fbd2dc274450907",
+    "url": "/movie_app/static/css/main.6c1dad89.chunk.css"
+  },
+  {
+    "revision": "f660bd658c12d94c2ada",
+    "url": "/movie_app/static/js/2.4ef8a755.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.4ef8a755.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b296fbd2dc274450907",
+    "url": "/movie_app/static/js/main.7ca140cb.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
